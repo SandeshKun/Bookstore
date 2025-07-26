be7f9eaa0877fe4cf3169abceaa77fe150ff9832
Sandesh Dhungana <sandeshstonedxd@gmail.com>
2025-07-26 14:41:07 +0545

    New urls added

--- stores/urls.py
+++ stores/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('books/', views.show_books, name='show_books'),
 ]
--- stores/views.py
+++ stores/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("It works!");
+    return HttpResponse("It works!")
+
+
+def show_books(request):
+    books = [
+        {"title": "Book 1", "author": "Author 1"},
+        {"title": "Book 2", "author": "Author 2"},
+        {"title": "Book 3", "author": "Author 3"},
+    ]
+    return render(request, 'books.html', {'books': books})
