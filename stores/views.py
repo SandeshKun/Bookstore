8aaa5cb40c4f64376741c82cfde64006a1f3cda0
Sandesh Dhungana <sandeshstonedxd@gmail.com>
2025-07-26 15:12:08 +0545

    Viwes

--- stores/views.py
+++ stores/views.py
@@ -1,29 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book
+from .models import Book, Author, Category
 
 def home(request):
-    # Show total books and latest additions
+    # Get statistics
     total_books = Book.objects.count()
-    latest_books = Book.objects.order_by('-created_at')[:3]
+    total_authors = Author.objects.count()
+    total_categories = Category.objects.count()
     
-    return render(request, 'home.html', {
+    # Get featured books (limit to 6)
+    featured_books = Book.objects.filter(
+        featured=True, 
+        status='available'
+    ).select_related('category').prefetch_related('authors')[:6]
+    
+    # Get latest books (limit to 6)
+    latest_books = Book.objects.filter(
+        status='available'
+    ).select_related('category').prefetch_related('authors').order_by('-created_at')[:6]
+    
+    # Get bestsellers (limit to 6)
+    bestsellers = Book.objects.filter(
+        bestseller=True,
+        status='available'
+    ).select_related('category').prefetch_related('authors')[:6]
+    
+    context = {
         'total_books': total_books,
-        'latest_books': latest_books
-    })
+        'total_authors': total_authors,
+        'total_categories': total_categories,
+        'featured_books': featured_books,
+        'latest_books': latest_books,
+        'bestsellers': bestsellers,
+    }
+    
+    return render(request, 'home.html', context)
 
 def show_books(request):
-    books = Book.objects.all().order_by('title')  # Alphabetical order
-    return render(request, 'books.html', {'books': books})
-
-def book_search(request):
-    query = request.GET.get('search', '')
-    if query:
-        books = Book.objects.filter(title__icontains=query)
-    else:
-        books = Book.objects.all()
+    books = Book.objects.filter(
+        status='available'
+    ).select_related('category').prefetch_related('authors').order_by('title')
     
-    return render(request, 'books.html', {
-        'books': books, 
-        'search_query': query
-    })
+    return render(request, 'books.html', {'books': books})
