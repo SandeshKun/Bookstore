d912276a840526f69b36215716690f23a5d2fed3
Sandesh Dhungana <sandeshstonedxd@gmail.com>
2025-07-26 14:36:33 +0545

    Added dockerfile/requiremets

--- Bookstore/settings.py
+++ Bookstore/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,9 +24,7 @@ SECRET_KEY = 'django-insecure-+e9d#gdxb*=5_3_=rm66&!_=@de&x=$*m7%z!o1jg+b(+#)beq
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -121,3 +119,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
